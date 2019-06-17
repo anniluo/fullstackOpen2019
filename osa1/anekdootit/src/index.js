870cbbf0fe8af95b40e613bedd831ce31e5f8f45
anniluo <anniluo@metropolia.fi>
2019-06-17 18:39:51 +0300

    1.12*: anekdootit step1

--- osa1/anekdootit/src/index.js
+++ osa1/anekdootit/src/index.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Laajenna sovellusta siten, että siihen tulee nappi, jota painamalla
+// sovellus näyttää satunnaisen ohjelmistotuotantoon liittyvän anekdootin
+const App = (props) => {
+    const [selected, setSelected] = useState(0)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    const handleClick = () => {
+        // randomize a number between 0 and the length of the anecdotes array
+        let randomNumber = Math.floor(Math.random() * anecdotes.length)
+        setSelected(randomNumber)
+        console.log('button clicked', randomNumber)
+    }
+
+    return (
+        <div>
+            {props.anecdotes[selected]}
+            <br/>
+            <button onClick={handleClick}>Change the anecdote</button>
+        </div>
+    )
+}
+
+const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(<App anecdotes={anecdotes}/>, document.getElementById('root'));
+
