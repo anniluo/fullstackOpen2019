a53dbafe2744d2fd91761335a00b603eed916c69
anniluo <anniluo@metropolia.fi>
2019-06-18 18:41:56 +0300

    2.5: separate module for component Course

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -1,63 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const Header = ({courseName}) => {
-    return (
-        <div>
-            <h1>{courseName}</h1>
-        </div>
-    )
-}
-
-const Part = (props) => {
-    return <p>{props.partName} {props.partExercises}</p>
-}
-
-const Content = ({parts}) => {
-
-    const partRows = () => parts.map(part => 
-        <Part 
-            key={part.id} 
-            partName={part.name} 
-            partExercises={part.exercises}
-        />
-    )
-
-    return (
-        <div>
-            {partRows()}
-        </div>
-    )
-}
-
-const Total = ({parts}) => {
-
-    const exercises = parts.map(part => part.exercises)
-    const total = exercises.reduce( (sum, exercises) => {
-        return sum + exercises
-    })
-
-    return (
-        <div>
-            <p>Total number of exercises: {total}</p>
-        </div>
-    )
-}
-
-const Course = ({courses}) => {
-
-    const courseRows = () => courses.map(course => {
-        return (
-            <div key={course.id}>
-                <Header courseName={course.name}/>
-                <Content parts={course.parts}/>
-                <Total parts={course.parts}/>
-            </div>
-        )
-    })
-
-    return courseRows()
-}
+import Course from './components/Course';
 
 const App = () => {
     const courses = [
