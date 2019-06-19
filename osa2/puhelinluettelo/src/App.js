2dead407d332cc1d4d523c67aa536adf18f6a22e
anniluo <anniluo@metropolia.fi>
2019-06-19 20:44:41 +0300

    2.8: puhelinluettelo step3

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,18 +2,20 @@ import React, { useState } from 'react';
 
 const App = () => {
     const [persons, setPersons] = useState([
-        {name: 'Mike Eagle'}
+        {name: 'Mike Eagle', number: '555 0505051'}
     ])
     const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
 
-    const addName = (event) => {
+    const addContact = (event) => {
         event.preventDefault()
         
         if (persons.find(person => person.name === newName)) {
             window.alert(`${newName} is already in the Phonebook!`)   
         } else {
             const nameObject = {
-                name: newName
+                name: newName,
+                number: newNumber
             }
             setPersons(persons.concat(nameObject))
         }
@@ -23,20 +25,27 @@ const App = () => {
         setNewName(event.target.value)
     }
 
-    const rows = () => persons.map(person => <li key={person.name}>{person.name}</li>)
+    const handleNumberChange = (event) => {
+        setNewNumber(event.target.value)
+    }
+
+    const rows = () => persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)
 
     return (
         <div>
             <h2>Phonebook</h2>
-            <form onSubmit={addName}>
+            <form onSubmit={addContact}>
                 <div>
-                    name <input value={newName} onChange={handleNameChange}/>
+                    name: <input value={newName} onChange={handleNameChange}/>
                 </div>
                 <div>
-                    <button type='submit'>add name</button>
+                number: <input value={newNumber} onChange={handleNumberChange}/>
+                </div>
+                <div>
+                    <button type='submit'>add contact</button>
                 </div>
             </form>
-            <h2>Numbers</h2>
+            <h2>Contacts</h2>
             <div>
                 <ul>
                     {rows()}
