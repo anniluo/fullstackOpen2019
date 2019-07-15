4e179257f7e67d38a8aa396b97cedaedb0a621b1
anniluo <anniluo@metropolia.fi>
2019-07-15 14:32:12 +0300

    4.1: blogilista, step1

--- osa4/bloglist/index.js
+++ osa4/bloglist/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const http = require('http')
 const express = require('express')
 const app = express()
@@ -14,7 +15,7 @@ const blogSchema = mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
+const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true })
 
 app.use(cors())
