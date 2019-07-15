8369f23fcd445dcd4c91a97ccfa8ed1f69e2dc3d
anniluo <anniluo@metropolia.fi>
2019-07-15 16:10:36 +0300

    4.2: blogilista, step2 (Divide given code to modules)

---
+++ osa4/bloglist/app.js
@@ -0,0 +1,21 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const blogRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
+    .then(() => {
+        console.log('Connected to Mongo Database')
+    })
+    .catch((error) => {
+        console.log('Error occured when connecting to Mongo Database:', error.message)
+    })
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use('/api/blogs', blogRouter)
+
+module.exports = app
