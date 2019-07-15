1a4168a18c80a75aada0f9a91248a5f41d209153
anniluo <anniluo@metropolia.fi>
2019-07-15 19:26:07 +0300

    4.4: apufunktioita ja yksikkötestejä, step2 (function to count total likes)

--- osa4/bloglist/utils/list_helper.js
+++ osa4/bloglist/utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = blogs => {
     return 1
 }
 
+const totalLikes = blogs => {
+    if (blogs.length === 0) {
+        return 0
+    }
+    
+    const likes = blogs.map(blog => blog.likes)
+    return likes.reduce((total, currentValue) => total + currentValue)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
