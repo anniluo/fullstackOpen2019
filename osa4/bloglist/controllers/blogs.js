5808b5a75d5a756548732b9b93ec0032fc130b00
anniluo <anniluo@metropolia.fi>
2019-07-18 20:28:00 +0300

    4.11*: blogilistan testit, step 4 (test for HTTP post to /api/likes for a blog with undefined likes)

--- osa4/bloglist/controllers/blogs.js
+++ osa4/bloglist/controllers/blogs.js
@@ -5,7 +5,7 @@ blogRouter.get('/', async (request, response) => {
     try {
         const blogs = await Blog.find({})
         response.status(200).json(blogs.map(blog => blog.toJSON()))
-        
+
     } catch(exception) {
         console.log('An error occured:', exception)
         response.status(404).end()
@@ -13,7 +13,13 @@ blogRouter.get('/', async (request, response) => {
 })
 
 blogRouter.post('/', async (request, response) => {
-    const blog = new Blog(request.body)
+    const body = request.body
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes === undefined ? 0 : body.likes 
+    })
 
     try {
         const savedBlog = await blog.save()
--- osa4/bloglist/tests/bloglist_api.test.js
+++ osa4/bloglist/tests/bloglist_api.test.js
@@ -6,28 +6,22 @@ const Blog = require('../models/blog')
 
 const initialBlogs = [
     {
-        _id: "5a422a851b54a676234d17f7",
         title: "React patterns",
         author: "Michael Chan",
         url: "https://reactpatterns.com/",
         likes: 7,
-        __v: 0
     },
     {
-        _id: "5a422aa71b54a676234d17f8",
         title: "Go To Statement Considered Harmful",
         author: "Edsger W. Dijkstra",
         url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
         likes: 5,
-        __v: 0
     },
     {
-        _id: "5a422b891b54a676234d17fa",
         title: "First class tests",
         author: "Robert C. Martin",
         url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
         likes: 10,
-        __v: 0
     },
 ]
 
@@ -54,11 +48,12 @@ test('Right amount of blogs are returned as json', async () => {
 })
 
 test('The identifier for a blog should be named "id"', async () => {
-    const response = await api
+    await api
         .get('/api/blogs')
         .expect(200)
         .expect('Content-Type', /application\/json/)
     
+    const response = await api.get('/api/blogs')
     response.body.forEach(blog => {
         expect(blog.id).toBeDefined()
     });
@@ -66,12 +61,10 @@ test('The identifier for a blog should be named "id"', async () => {
 
 test('A valid blog can be added', async () => {
     const newBlog = {
-        _id: "5a422a851b54a123464d17f7",
         title: "Automating My Todo with GitHub and Twilio",
         author: "Alice Goldfuss",
         url: "https://blog.alicegoldfuss.com/automating-my-todo/",
-        likes: 42,
-        __v: 0
+        likes: 42
     }
 
     await api
@@ -84,6 +77,22 @@ test('A valid blog can be added', async () => {
     const urls = response.body.map(blog => blog.url)
     expect(response.body.length).toBe(initialBlogs.length + 1)
     expect(urls).toContain(newBlog.url)
+})
+
+test('A blog with no likes gets a value of 0 for that', async () => {
+    const newBlog = {
+        title: "Automating My Todo with GitHub and Twilio",
+        author: "Alice Goldfuss",
+        url: "https://blog.alicegoldfuss.com/automating-my-todo/",
+    }
+
+    const response = await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+    
+    expect(response.body.likes).toBe(0)
 })
 
 afterAll(() => {
